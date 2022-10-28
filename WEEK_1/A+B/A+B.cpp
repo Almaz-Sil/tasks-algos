f9257ffcb5bf3e5f5d095ccc1a76b041d25dc32b
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-10-28 03:11:41 +0300

    Задания 1 недели

---
+++ WEEK_1/A+B/A+B.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream> 
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a, b;
+    std::cout << "Введите значение а: ";
+    std::cin >> a;
+    std::cout << "\n" << "Введите значение b: ";
+    std::cin >> b;
+    std::cout << a + b;
+    return 0;
+}
