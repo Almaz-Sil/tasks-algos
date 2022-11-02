4cc961cb0cdd0127017d108a3247c7ca5151a8f3
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-11-02 15:37:03 +0300

    Division

---
+++ WEEK_1/Division/Division.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	cout << "a = ";
+	cin >> a;
+	cout << "b = ";
+	cin >> b;
+
+	if (b == 0) cout << "Impossible";
+	else {
+		cout << a / b;
+	}
+}
+
