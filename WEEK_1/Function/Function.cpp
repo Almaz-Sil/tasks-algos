83f93c197a738d5d3bcc1ad0530f30ab7853b6c8
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-11-02 14:58:25 +0300

    Function

---
+++ WEEK_1/Function/Function.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double Disc(double a, double b, double c) {
+	return sqrt(b * b - 4 * a * c);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	double a, b, c;
+	double D;
+	double x1, x2;
+	cout << "введите a:";
+	cin >> a;
+	cout << "\n" << "введите b:";
+	cin >> b;
+	cout << "\n" << "введите c:";
+	cin >> c;
+
+	if (a != 0) {
+		D = Disc(a, b, c);
+		if (D > 0) {
+			x1 = (-b - D) / (2 * a);
+			x2 = (-b + D) / (2 * a);
+			cout << "x1: " << x1 << "\n" << "x2: " << x2;
+		}
+		else if (D == 0) {
+			x1 = (-b - D) / (2 * a);
+			cout << "x: " << x1;
+		}
+		else {
+			cout << "корней нет";
+		}
+	}
+	else {
+		x1 = -c / b;
+		cout << "x: " << x1;
+	}
+}
--- WEEK_1/Min_str/Min_str.cpp
+++ WEEK_1/Min_str/Min_str.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 int main()
 {
-    vector<string> words = { "fire", "fog", "wood"};
-    string min = words[0];
+    //vector<string> words = { "fire", "fog", "wood"};
+    string words = "fire fog wood";
+    string min = "wood";
 
     for(auto i : words) {
         if (i < min)
