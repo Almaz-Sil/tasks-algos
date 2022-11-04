73bb5aee40c4bbf54b30ea83c060ca7ea3d2282b
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-11-04 20:54:23 +0300

    четные числа
    Вывод всех четных чисел в данном отрезке

---
+++ WEEK_1/Even numbers/Even numbers.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b;
+    vector <int> res;
+
+    cout << "Введите начальное число: ";
+    cin >> a;
+    cout << "\n" << "Введите конечное число: ";
+    cin >> b;
+
+    if (a % 2 == 1) a++;
+
+    for (int i = a; i <= b; i += 2) {
+        res.push_back(i);
+    }
+
+    for (int i : res) cout << i << "\n";
+}
