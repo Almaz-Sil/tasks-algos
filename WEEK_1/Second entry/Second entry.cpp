e6e042894f3f5fbe825dc99be05007ee4be0a2a7
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-11-04 21:50:31 +0300

    второе буквы f

---
+++ WEEK_1/Second entry/Second entry.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() //выводить индекс второго вхождения буквы f
+{
+    setlocale(LC_ALL, "Russian");
+    string word;
+    int fc = -1, ind;
+    cout << "Введите любое слово на ангийлском: ";
+    cin >> word;
+
+    for (int i = 0; i < word.size(); i++) {
+        if (word[i] == 'f') {
+            fc++;
+            if (fc >= 2) break;
+            else ind = i;
+        }
+    }
+    
+    if (fc == -1) cout << -2;
+    else if (fc == 0) cout << -1;
+    else cout << ind;
+}
