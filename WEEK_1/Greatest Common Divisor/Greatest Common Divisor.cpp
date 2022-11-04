b973d7fe127ca7d14602b92936b49bac58a84649
Almaz-Sil <90396673+Almaz-Sil@users.noreply.github.com>
2022-11-04 22:10:44 +0300

    максимальный общий делитель

---
+++ WEEK_1/Greatest Common Divisor/Greatest Common Divisor.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a, b, max = 1;
+
+    cout << "Ведите два наутральных числа:\n" << "а = ";
+    cin >> a;
+    cout << "b = ";
+    cin >> b;
+
+    for (int i = 1; i <= a && b; i++) {
+        if (a % i == 0 && b % i == 0) max = i;
+    }
+
+    cout << "максимальный общий делитель: " << max;
+}
